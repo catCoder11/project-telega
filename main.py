c4ee87d9f26e75211c71b7c65f72a9a9074ff99b
Dmitry Kvasnikov <kvasnikov.dmitry@gmail.com>
2024-04-01 20:48:22 +0700

    test

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
-print("world")
+from data import db_session
+from flask import Flask, render_template, redirect
+from data.users import User
+from forms.user import RegisterForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+# @app.route("/")
+# def index():
+#     db_sess = db_session.create_session()
+#     news = db_sess.query(News).filter(News.is_private != True)
+#     return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    print(1)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host="127.0.0.1")
+
+if __name__ == '__main__':
+    main()
