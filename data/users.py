94fdfda900858d06ad740a6da440ce6525bf3239
Dmitry Kvasnikov <kvasnikov.dmitry@gmail.com>
2024-04-05 15:47:57 +0700

    users beta

---
+++ data/users.py
@@ -0,0 +1,24 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    telegram_id = sqlalchemy.Column(sqlalchemy.String, nullable=True, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    access = orm.relationship('Access', back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
