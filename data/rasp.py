3f33e8c1049d992d3427164ee0be00c9f41186d5
Dmitry Kvasnikov <kvasnikov.dmitry@gmail.com>
2024-04-05 15:49:02 +0700

    rasp beta

---
+++ data/rasp.py
@@ -0,0 +1,31 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+class Rasp(SqlAlchemyBase):
+    __tablename__ = 'rasp'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    modify_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    creator = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    start_time = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    klass_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("klasses.id"), nullable=True)
+    klass = orm.relationship('K_klass')
+    auditory_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("auditory.id"), nullable=True)
+    auditory = orm.relationship('K_auditory')
+    subject_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("subjects.id"), nullable=True)
+    subject = orm.relationship('K_subject')
+    teacher_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("teachers.id"), nullable=True)
+    teacher = orm.relationship('K_teacher')
+    task = orm.relationship('Task', back_populates="rasp")
+    # rasp_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("rasp.id"), nullable=True)
+    # rasp = orm.relationship('Rasp')
+    # task_type_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("rasp.id"), nullable=True)
+    # task_type = orm.relationship('K_task_type')
+    # descriptions = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    # modify_date = sqlalchemy.Column(sqlalchemy.DateTime,
+    #                                  default=datetime.datetime.now)
+    # creator = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    # f_to_t = orm.relationship('K_file_to_task', back_populates='task')
